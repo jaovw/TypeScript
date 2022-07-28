a9f6824679353aa5f7623dccc4e761de1ec57f35
jaovw <joojvw@gmail.com>
2022-07-27 21:41:41 -0300

    tipos

--- introducao/src/basico.js
+++ introducao/src/basico.js
@@ -1,2 +1,14 @@
 var a = 'TypeScript compilado novo com run dev !';
 console.log(a);
+// TIPOS
+// NUMEROS
+var numero = 10;
+console.log(numero);
+// STRING
+var nome = 'Joao';
+console.log(nome);
+// BOOLEAN
+var estaLigado = false;
+console.log(estaLigado);
+// CASO DECLARE A VARIAVEL SEM ATRIBUIR NADA EXPLICITAMENTE
+// ASSUME O TIPO __ ANY
--- introducao/src/basico.ts
+++ introducao/src/basico.ts
@@ -1,2 +1,19 @@
 const a: string =  'TypeScript compilado novo com run dev !'
 console.log(a)
+
+// TIPOS
+
+// NUMEROS
+const numero: number = 10
+console.log(numero)
+
+// STRING
+const nome: string = 'Joao'
+console.log(nome)
+
+// BOOLEAN
+const estaLigado: boolean = false
+console.log(estaLigado)
+
+// CASO DECLARE A VARIAVEL SEM ATRIBUIR NADA EXPLICITAMENTE
+// ASSUME O TIPO __ ANY
